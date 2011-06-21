82e36114299c718994b7f644cbd826af89b4e496
Cat Chen <cathsfz@gmail.com>
2011-06-21 09:33:04 +0800

    added redis client to web.js

--- web.js
+++ web.js
@@ -1,4 +1,20 @@
-var express = require('express');
+const express = require('express');
+const redis = require('redis');
+const url = require('url');
+
+var redisClient = getRedisClient();
+
+var getRedisClient = function() {
+    var redisClient;
+    if (process.env.REDISTOGO_URL) {
+        var redisURL = url.parse(process.env.REDISTOGO_URL);
+        redisClient = redis.createClient(rtg.port, rtg.hostname);
+        redisClient.auth(redisURL.auth.split(":")[1]);
+    } else {
+        redisClient = redis.createClient();
+    }
+    return redisClient;
+};
 
 var app = express.createServer(express.logger());
 
